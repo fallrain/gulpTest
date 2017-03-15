b4c9f1575db2ff9b22d3667b862527a1224c635d
lenovo <416266475@qq.com>
2017-03-15 18:33:05 +0800

    init

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * 说明：
+ */
+function test(name, age){
+  console.log('名字：' + name);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,100 @@
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-gulp.task('test1', function(){
-  console.log('这是一个测试任务，没有其他');
-});
-gulp.task('sass', function(){
-  return gulp.src('app/css/**/*.+(scss|sass)')
+var browserSync = require('browser-sync');
+var gulpIf = require('gulp-if');
+var minCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var runSequence = require('run-sequence');
+var gulpCfgObj = {
+  allSrc : 'app/**',
+  sassSrc : 'app/css/**/*.+(scss|sass)',//sass文件目录
+  cssDist : 'dist/css/',
+  jsSrc : 'app/js/**/*.js',//js文件目录
+  jsDist : 'dist/js/',//js文件目录
+  htmlSrc : 'app/html/**/*.+(html|shtml)',//html文件目录
+  htmlDist : 'dist/html/'
+};
+//转化css
+gulp.task('sassTk', function(){
+  return gulp.src(gulpCfgObj.sassSrc)
     .pipe(sass())
-    .pipe(gulp.dest('dist/css/'));
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.reload({
+      stream : true
+    }));
+});
+
+gulp.task('browserSyncTk', function(){
+  browserSync({
+    server : {
+      baseDir : './',
+      directory : true
+    }
+  });
+});
+gulp.task('watchSassTk', function(){
+  //监控scss的变化，自动编译
+  gulp.watch(gulpCfgObj.sassSrc, function(){
+    gulp.run('bulidCss');
+    browserSync.reload();
+  });
+});
+gulp.task('watchHtmlTk', function(){
+  gulp.watch(gulpCfgObj.htmlSrc, function(){
+    gulp.run('bulidHtml');
+    browserSync.reload();
+  });
+});
+
+gulp.task('watchJsTk', function(){
+  gulp.watch(gulpCfgObj.jsSrc, function(){
+    gulp.run('bulidJs');
+  });
+});
+gulp.task('cleanDist', function(){
+  del('dist');
+});
+gulp.task('bulidHtml', function(){
+  gulp.src(gulpCfgObj.htmlSrc)
+    .pipe(gulp.dest(gulpCfgObj.htmlDist))
+});
+
+gulp.task('bulidJs', function(){
+  gulp.src(gulpCfgObj.jsSrc)
+    .pipe(uglify())
+    .pipe(gulp.dest(gulpCfgObj.jsDist))
+});
+
+gulp.task('bulidCss', function(){
+  gulp.src(gulpCfgObj.sassSrc)
+    .pipe(sass())
+    .pipe(minCss())
+    .pipe(gulp.dest(gulpCfgObj.cssDist));
+});
+
+gulp.task('bulidDist', function(){
+  /*把各个文件编译压缩后输出到dist*/
+  runSequence('cleanDist', ['bulidHtml', 'bulidJs', 'bulidCss']);
+});
+gulp.task('autoRl', function(){
+  browserSync(gulpCfgObj.allSrc, function(){
+    browserSync.reload({
+      stream : true
+    });
+  })
+});
+
+gulp.task('watchTk', function(){
+  /*监控任务*/
+  gulp.run('watchSassTk');
+  gulp.run('watchJsTk');
+  gulp.run('watchHtmlTk');
+
+});
+gulp.task('debug', function(){
+  gulp.run('bulidDist');
+  gulp.run('watchTk');
+  gulp.run('browserSyncTk');
 });
